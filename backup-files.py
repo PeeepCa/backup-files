a704737b057e73a6844635dd78319f705c6b610c
Josef Pokorný <155643137+PeeepCa@users.noreply.github.com>
2024-03-21 10:38:55 +0100

    Add files via upload

--- backup-files.py
+++ backup-files.py
@@ -1,38 +1,57 @@
-# Sync of files - DONE
-# Sync should be one way - DONE
-# Sync should be done periodically - DONE
-# File manipulation should be logged to file and console - CONSOLE
-# Folder paths, interval and log path should be configurable via args - DONE
-# Folder sync should not use 3th party library - DONE
-# Other things can be via libraries - DONE
+# Backup-files
+
+# Application just checking source folder if there s any new file or if there s any file which has changes
+# in comparison to second folder
+# App checking the creation time and date to compare files
+# if there s any change in destination folder it ll automaticly revert changes to actual one
+# Interval is implemented just by adding sleep
+# App can be improved by multithreading in future, but it depends on future testing
+# Multithreading, can improve logging performance
+# Theres possibility to add UI as well
 
 import sys
 import os
-import shutil
-import time
+from time import sleep
+from shutil import copy2
+from datetime import datetime
 
 class main:
     def __init__(self):
         # Args: Path to sync, Synced path, Interval, Log
         try:
-            self.args = sys.argv
             self.src = sys.argv[1]
             self.dst = sys.argv[2]
             self.interval = int(sys.argv[3])
             self.log = sys.argv[4]
+            print('Parameters are:\nSource path: ' + self.src + '\nDestination path: ' + self.dst + '\nInterval: ' + str(self.interval) + 's\nLog path: ' + sys.argv[4] + '\nApp started at: ' + self.timeStmp())
+            self.logHandler(self.timeStmp() + ' Application started with parameters:\nSource path: ' + self.src + '\nDestination path: ' + self.dst + '\nInterval: ' + str(self.interval) + 's\nLog path: ' + sys.argv[4])
         except IndexError:
             # Error with missing parameters
             print('No parameters given! Parameters should be: Sync folder path, Syndec folder, Interval, Log file.')
 
+    def logHandler(self, content):
+        # Log handler
+        self.config = open(self.log, 'a')
+        self.config.write(content + '\n')
+        self.config.close()
+
+    def timeStmp(self):
+        # Timestamp for logging
+        actualTime = datetime.now()
+        actualTime = actualTime.strftime('%d/%m/%Y, %H:%M:%S')
+        return actualTime
+
     def run(self):
+        # Main loop for backup service
         while True:
-            print('1')
+            print('Alive')
             try:
                 src_d = os.listdir(self.src)
                 src_d.sort()
                 dst_d = os.listdir(self.dst)
                 dst_d.sort()
 
+                # Added because one or second folder can have different number of files in
                 if len(src_d) < len(dst_d):
                     size = len(dst_d)
                 else:
@@ -45,25 +64,28 @@ class main:
                                 continue
                             else:
                                 # If source file was different then copy new one
-                                shutil.copy2(self.src + src_d[i],self.dst + src_d[i])
+                                copy2(self.src + src_d[i],self.dst + src_d[i])
                                 print('File copied because file difference '+ self.src + src_d[i] + ' to ' + self.dst + '!')
+                                self.logHandler(self.timeStmp() + ' File copied because file difference '+ self.src + src_d[i] + ' to ' + self.dst)
                         else:
                             # If there s missing file, then copy new one
-                            shutil.copy2(self.src + src_d[i],self.dst + src_d[i])
+                            copy2(self.src + src_d[i],self.dst + src_d[i])
                             print('File copied because they are missing '+ self.src + src_d[i] + ' to ' + self.dst + '!')
+                            self.logHandler(self.timeStmp() + ' File copied because they are missing '+ self.src + src_d[i] + ' to ' + self.dst)
                     except IndexError:
                         pass
                     try:
                         if dst_d[i] not in src_d:
                             # if there s file which doesnt belong there then delete it
-                            print('File deleted ' + self.dst + dst_d[i])
                             os.remove(self.dst + dst_d[i])
+                            print('File deleted ' + self.dst + dst_d[i])
+                            self.logHandler(self.timeStmp() + ' File deleted ' + self.dst + dst_d[i])
                     except IndexError:
                         pass
             except AttributeError:
                 # Pass because duplicate error
                 pass
-            time.sleep(self.interval)
+            sleep(self.interval)
 
 if __name__ == '__main__':
     main = main()
